97ff2cccba55a3c86e0c46d78b144bb4b5b796e8
amansingh057 <amansingh.95409540@gmail.com>
2022-10-12 23:05:04 +0530

    added

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,34 +2,48 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname+"/date.js")
+
+
 const app = express()
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 var items =["Buy Food","Cook The food","Eat the food"];
+var WorkItems = []
 app.get("/",function(req,res){
-    var today = new Date()
-    day = "weekend"
-    // var arr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    var options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    }
-    var day = today.toLocaleDateString("en-US",options)
+    let day = date.getDate()
     // if(today.getDay()== 6 || 0)
     //     day = "weekend!"
     // }else{
     //     day = "weekday"
     // }
     // day = arr[today.getDay()]
-    res.render('list',{Kindofday:day,newListItem:items});
+    res.render('list',{ListTitle: day,newListItem:items});
 })
+
+
+
+app.get("/work",function(req,res){
+    res.render("list",{ListTitle: "Work List",newListItem:WorkItems});
+})
+
+app.get("/about",function(req,res){
+    res.render("about")
+})
+
 app.post('/',function(req,res){
+    console.log(req.body)
     var item = req.body.newItem
-    items.push(item)
-    res.redirect('/');
+    if(req.body.list ==="Work"){
+        WorkItems.push(item)
+        res.redirect('/work');
+    }else{
+        items.push(item)
+        res.redirect('/');
+    }
 })
 
 app.listen(process.env.PORT || 3000,function(){
