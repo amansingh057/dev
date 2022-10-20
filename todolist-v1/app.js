545a5759a5ea1834d379303145e58eb0b933b37c
amansingh057 <amansingh.95409540@gmail.com>
2022-10-20 23:25:38 +0530

    added

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -122,6 +122,11 @@ app.post("/delete", function (req, res) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Running at port 3000");
 });
