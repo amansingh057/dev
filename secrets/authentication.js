31f09179742822fe58867126d2ffd868673a94a2
amansingh057 <amansingh.95409540@gmail.com>
2022-10-27 23:31:27 +0530

    Login with google

--- secrets/authentication.js
+++ secrets/authentication.js
@@ -7,8 +7,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+// const encrypt = require('mongoose-encryption')
+// const md5 = require('md5')
+// const bcrypt = require('bcrypt'); // Helping in Salting the password
+// const saltRound = 10;
 
 const app = express();
 
@@ -35,11 +37,9 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId:String
   // whatever else
 });
 userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
 
 // userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
@@ -47,56 +47,12 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function (user, cb) {
-  process.nextTick(function () {
-    return cb(null, {
-      id: user.id,
-      username: user.username,
-      picture: user.picture,
-    });
-  });
-});
-
-passport.deserializeUser(function (user, cb) {
-  process.nextTick(function () {
-    return cb(null, user);
-  });
-});
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home");
 });
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile"] })
-);
-
-app.get(
-  "/auth/google/secrets",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/secrets");
-  }
-);
-
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -151,6 +107,49 @@ app.post("/login", function (req, res) {
     }
   });
 });
+
+/* ******************* With The Help of Bcrypt Method ******/
+
+// app.post('/register',function(req,res){
+//     bcrypt.hash(req.body.password, saltRound, function(err, hash) {
+//         // Store hash in your password DB.
+//         const newUser = new User({
+//             email:req.body.username,
+//             // password: md5(req.body.password)
+//             password: hash
+//         })
+//         newUser.save(function(err){
+//             if(err){
+//                 console.log(err)
+//             }else{
+//                 res.render('secrets')
+//             }
+//         })
+//     });
+
+// })
+
+// app.post('/login',function(req,res){
+//     const username = req.body.username
+//     // const password = md5(req.body.password)
+//     const password = req.body.password
+
+//     User.findOne({email:username},function(err,foundUser){
+//         if(err){
+//             console.log(err)
+//         }else{
+//             if(foundUser){
+//                 // if(foundUser.password==password){
+//                 //     res.render('secrets')
+//                 // }
+//                 bcrypt.compare(password, foundUser.password, function(err, result) {
+//                     if(result==true)
+//                         res.render('secrets')
+//                 });
+//             }
+//         }
+//     })
+// })
 
 app.listen(3000, function () {
   console.log("Server running at  port 3000");
